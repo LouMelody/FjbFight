6815f961124c97fc9988a9e8c0bc951726407c5f
LouMelody <kamatatu_co_vm6ae@yahoo.co.jp>
2022-10-08 13:26:12 +0900

    AI mock added

---
+++ FinalFightGit/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    enum EnemyState
+    {
+        Wait,
+        Walk,
+        Chase,
+        Attack,
+    }
+    EnemyState enemyState = EnemyState.Wait;
+    bool coroutineRunning = false;
+    private void Update()
+    {
+        enemyState = MainRoutine();
+        if (coroutineRunning) return;
+
+        switch(enemyState)
+        { 
+            case EnemyState.Wait:
+                Debug.Log("wait");
+                break;
+            case EnemyState.Walk:
+                Debug.Log("walk");
+                break;
+            case EnemyState.Chase:
+                Debug.Log("chase");
+                break;
+            case EnemyState.Attack:
+                Debug.Log("attack");
+                break;
+        }
+        StartCoroutine(nameof(AITimer));
+    }
+    // inner
+    EnemyState MainRoutine()
+    {
+        int rand = Random.Range(0, 3);
+        return rand switch
+        {
+            0 => EnemyState.Wait,
+            1 => EnemyState.Walk,
+            2 => EnemyState.Chase,
+            3 => EnemyState.Attack,
+        };
+    }
+    IEnumerator AITimer()
+    {
+        coroutineRunning = true;
+        yield return new WaitForSeconds(10f);
+        coroutineRunning = false;
+    }
+}
