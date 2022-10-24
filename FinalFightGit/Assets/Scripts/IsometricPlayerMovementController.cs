0e3229b8ea567443b054740bd0d26588b2e01bb8
LouMelody <kamatatu_co_vm6ae@yahoo.co.jp>
2022-10-24 17:54:31 +0900

    add jump script

--- FinalFightGit/Assets/Scripts/IsometricPlayerMovementController.cs
+++ FinalFightGit/Assets/Scripts/IsometricPlayerMovementController.cs
@@ -1,15 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using static UnityEngine.Mathf;
 public class IsometricPlayerMovementController : MonoBehaviour
 {
 
     public float movementSpeed = 1f;
+    public float jumpSpeed = 3f;
     public AnimCon animCon;
 
-    Rigidbody2D rbody;
-
+    private Rigidbody2D rbody;
+    private bool isJump = false;
     private void Awake()
     {
         rbody = GetComponent<Rigidbody2D>();
@@ -20,6 +21,7 @@ public class IsometricPlayerMovementController : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.Space))
         {
             animCon.PlayJump();
+            StartCoroutine(nameof(Jump));
         }
         if (Input.GetKeyDown(KeyCode.L))
             animCon.PlayPunch();
@@ -35,10 +37,43 @@ public class IsometricPlayerMovementController : MonoBehaviour
         float verticalInput = Input.GetAxis("Vertical");
 
         Vector2 inputVector = new Vector2(horizontalInput, verticalInput);
-        animCon.ChangeDirection(inputVector.magnitude);
+        if(!isJump) animCon.ChangeDirection(inputVector.magnitude);
         inputVector = Vector2.ClampMagnitude(inputVector, 1);
         Vector2 movement = inputVector * movementSpeed;
         Vector2 newPos = currentPos + movement * Time.fixedDeltaTime;
-        rbody.MovePosition(newPos);
+        if(!isJump) rbody.MovePosition(newPos);
+    }
+
+    private IEnumerator Jump()
+    {
+        if (isJump) yield break;
+
+        yield return null;
+
+        isJump = true;
+        float elapsed = 0f;
+        while (true)
+        {
+            if (elapsed <= 0.5f)
+            {
+                var posY = transform.position.y;
+                var newPos = new Vector2(transform.position.x, posY + Abs(posY * jumpSpeed * Time.deltaTime));
+                rbody.MovePosition(newPos);
+                elapsed += Time.deltaTime;
+            }
+            else if (elapsed <= 1f)
+            {
+                var posY = transform.position.y;
+                var newPos = new Vector2(transform.position.x,  posY + -Abs(posY * -jumpSpeed * Time.deltaTime));
+                rbody.MovePosition(newPos);
+                elapsed += Time.deltaTime;
+            }
+            else
+            {
+                isJump = false;
+                yield break;
+            }
+            yield return null;
+        }
     }
 }
